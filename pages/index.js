60b4175167ceab3db22d018356ab65433c18a0d2
Anhad <84476084+07anhad@users.noreply.github.com>
2022-07-22 00:35:36 +0530

    first

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+import Image from 'next/image';
+
+import { PostCard, Categories, PostWidget } from '../components';
+
+const posts = [
+  { title: 'React Testing', excerpt: 'Learn React Testing' },
+  { title: 'React with Tailwind', excerpt: 'Learn ReactJs with tailwindCSS' }
+]
+
+export default function Home() {
+  return (
+    <div className="container mx-auto px-10 mb-8 bg-gray-300">
+      <Head>
+        <title>Anhad Blogs</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post) => (
+            <PostCard post={post} key={post.title}/>
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories/>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
