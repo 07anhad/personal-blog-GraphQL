2d9510e0f8e7c90ab2367d0730c5f2e08106eefc
Anhad <84476084+07anhad@users.noreply.github.com>
2022-07-24 01:58:43 +0530

    new post through gql

--- components/Header.jsx
+++ components/Header.jsx
@@ -13,8 +13,8 @@ const Header = () => {
           <div className="border-b w-full inline-block border-blue-400 py-8">
               <div className="md:float-left block">
                   <Link href="/">
-                      <span className="cursor-pointer font-bold text-4xl text-white">
-                          Anhad Blogs
+                      <span className="cursor-pointer font-bold font-serif text-4xl text-white">
+                        Anhad's Blog
                       </span>
                   </Link>
               </div>
