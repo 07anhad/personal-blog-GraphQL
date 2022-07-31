5e2711ee40e420959a421619008dd2b469e041e6
Anhad <84476084+07anhad@users.noreply.github.com>
2022-08-01 01:34:33 +0530

    start with the comments form

--- services/index.js
+++ services/index.js
@@ -41,6 +41,41 @@ export const getPosts = async () => {
   return result.postsConnection.edges;
 };
 
+
+export const getPostDetails = async (slug) => {
+  const query = gql`
+  query GetPostDetails($slug : String!) {
+    post(where: {slug: $slug}) {
+      title
+      excerpt
+      featuredImage {
+        url
+      }
+      author{
+        name
+        bio
+        photo {
+          url
+        }
+      }
+      createdAt
+      slug
+      content {
+        raw
+      }
+      categories {
+        name
+        slug
+      }
+    }
+  }
+  `;
+  
+  const result = await request(graphqlAPI, query, { slug });
+  
+  return result.post;
+};
+
 export const getCategories = async () => {
   const query = gql`
     query GetGategories {
@@ -54,40 +89,6 @@ export const getCategories = async () => {
   const result = await request(graphqlAPI, query);
 
   return result.categories;
-};
-
-export const getPostDetails = async (slug) => {
-  const query = gql`
-    query GetPostDetails($slug : String!) {
-      post(where: {slug: $slug}) {
-        title
-        excerpt
-        featuredImage {
-          url
-        }
-        author{
-          name
-          bio
-          photo {
-            url
-          }
-        }
-        createdAt
-        slug
-        content {
-          raw
-        }
-        categories {
-          name
-          slug
-        }
-      }
-    }
-  `;
-
-  const result = await request(graphqlAPI, query, { slug });
-
-  return result.post;
 };
 
 export const getSimilarPosts = async (categories, slug) => {
@@ -106,7 +107,7 @@ export const getSimilarPosts = async (categories, slug) => {
       }
     }
   `;
-  const result = await request(graphqlAPI, query)
+  const result = await request(graphqlAPI, query,{categories,slug})
 
   return result.posts;
 }
